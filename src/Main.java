85489e79e1645ff3510ad3e069ca9b63f3fe1bcd
ArsenYeghikyan <arsegikyan@gmail.com>
2020-01-20 18:33:21 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+//    public static void main(String[] args) {
+        long x = 1454;
+       static  {
+            long x = 1454;
+           System.out.println(x);
+           System.out.println();
+
+       }
+//    }
+}
